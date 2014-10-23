f824a6c5d62ee9045f984dd06b67a36db107b3d6
Cezar Cretu <cretucezarandrei@gmail.com>
2014-10-23 18:42:36 +0300

    added basic interfaces

---
+++ EventCar/EventCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCar
+{
+    public static class EventCar
+    {
+        public static IEventDispatcher Dispatcher { get; set; }
+
+        public static void Fire<TEvent>(TEvent ev) where TEvent : IEvent
+        {
+            Dispatcher.Dispatch(ev);
+        }
+
+        public static void Register<TEvent>(TEvent ev) where TEvent : IEvent
+        {
+            Dispatcher.Register(ev);
+        }
+    }
+}
---
+++ EventCar/IEventDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCar
+{
+    public interface IEventDispatcher
+    {
+        void Dispatch<TEvent>(TEvent ev) where TEvent : IEvent;
+
+        void Register<TEvent>(TEvent ev) where TEvent : IEvent;
+    }
+}
