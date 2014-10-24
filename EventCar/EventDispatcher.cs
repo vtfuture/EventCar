cf5c94e1250d9609bc8ac990ce1770af640b0c23
Cezar Cretu <cretucezarandrei@gmail.com>
2014-10-24 13:48:19 +0300

    added support for multiple handler for the same event

--- EventCar/EventDispatcher.cs
+++ EventCar/EventDispatcher.cs
@@ -10,24 +10,22 @@ namespace EventCar
 {
     public class EventDispatcher : IEventDispatcher
     {
-        private Dictionary<Type, Type> mappings = new Dictionary<Type, Type>();
+        private Dictionary<Type, List<Type>> mappings = new Dictionary<Type, List<Type>>();
 
         public void Dispatch<TEvent>(TEvent ev) where TEvent : IEvent
         {
             var eventType = ev.GetType();
 
-            if (!mappings.ContainsKey(eventType))
+            var handlerTypes = GetMappings(eventType);
+
+            foreach (var handlerType in handlerTypes)
             {
-                throw new NotSupportedException("No handler implemented for type " + eventType.Name);
+                var instance = ServiceLocator.Current.GetInstance(handlerType);
+
+                var handler = instance as IEventHandler<TEvent>;
+
+                handler.Handle(ev);    
             }
-
-            var handlerType = mappings[eventType];
-
-            var instance = ServiceLocator.Current.GetInstance(handlerType);
-
-            var handler = instance as IEventHandler<TEvent>;
-
-            handler.Handle(ev);
         }
 
         public void Register<TEvent, THandler>() where TEvent : IEvent
@@ -35,8 +33,33 @@ namespace EventCar
             var eventType = typeof(TEvent);
             var handlerType = typeof(THandler);
 
-            mappings.Add(eventType, handlerType);
-            
+            AddMapping(eventType, handlerType);
+        }
+
+        private List<Type> GetMappings(Type eventType)
+        {
+            if (!mappings.ContainsKey(eventType))
+            {
+                throw new NotSupportedException("No handler implemented for type " + eventType.Name);
+            }
+
+            return mappings[eventType];
+        }
+
+        private void AddMapping(Type eventType, Type handlerType)
+        {
+            List<Type> typeList = null;
+            if (!mappings.ContainsKey(eventType))
+            {
+                typeList = new List<Type>();
+                mappings.Add(eventType, typeList);
+            }
+            else
+            {
+                typeList = mappings[eventType];
+            }
+
+            typeList.Add(handlerType);
         }
     }
 }
